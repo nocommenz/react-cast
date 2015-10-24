dc4bf47fa456a5f773a5fad0fae26e8fe3a164fe
Piti Pitiaunchamroen <nocommenz@gmail.com>
2015-10-24 20:59:16 +0700

    Intro to state

--- src/app.jsx
+++ src/app.jsx
@@ -1,21 +1,14 @@
 var React = require('react');
-var ThumbnailList = require('./thumbnail-list');
+var Dropdown = require('./dropdown');
 
 var options = {
-  thumbnailData: [{
-    title: 'Show Courses',
-    number: 32,
-    header: 'Learn React',
-    description: 'React is fantastic new library for making fast, dynamic pages.',
-    imageUrl: 'http://formatjs.io/img/react.svg'
-  }, {
-    title: 'Show Courses',
-    number: 25,
-    header: 'Learn Gulp',
-    description: 'Gulp will speedup your environment workflow',
-    imageUrl: 'https://avatars0.githubusercontent.com/u/6200624?v=3&s=400'
-  }]
-};
+  title: 'Choose a dessert',
+  items: [
+    'Apple Pie',
+    'Peach Cabbler',
+    'Coconut Cream Pie'
+  ]
+}
 
-var element = React.createElement(ThumbnailList, options);
+var element = React.createElement(Dropdown, options);
 React.render(element, document.querySelector('.target'));
